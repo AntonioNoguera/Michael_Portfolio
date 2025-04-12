11bcedcef2020db55d09abca8318d15c7c982d82
Michael Guzmán <116075308+AntonioNoguera@users.noreply.github.com>
2025-04-12 12:01:23 -0600

    Feat [Row oriented Contact Me]

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -29,6 +29,7 @@ export default function HomePage() {
       <WebSection/>
       
       <ContactSection/>
+      
     </motion.div>
   )
 }
--- src/app/[locale]/sections/contact_me/contact_me.tsx
+++ src/app/[locale]/sections/contact_me/contact_me.tsx
@@ -5,30 +5,60 @@ import React from "react";
 
 import Footer from "@/components/Footer";
 import TecnologyCard from "../web/components/tecnology_card";
+import { motion } from "framer-motion";
 
 export default function ContactSection() {
 
     const t = useTranslations('Contact_Section');
 
     return (
-        <section id="contact" className="h-screen snap-start bg-secondary flex flex-col items-center justify-center pt-16">
+        <section id="contact" className="h-screen snap-start bg-secondary flex flex-col items-center justify-center pt-20">
             <p className="text-page_title font-bold text-center text-black_primary">
                 {t('title')}
             </p>
 
-            <div className="flex-grow flex items-center justify-center w-full">
+            <div className="flex-grow flex flex-col items-center justify-center w-full">
+
                 <p className="text-body2 text-center px-28 pt-6 text-black_primary">
                     {t('subtitle')}
                 </p>
-            </div>
 
-            <div className="flex flex-wrap w-full justify-center items-center items-stretch">
+                <div className="flex flex-wrap w-full justify-center items-center items-stretch">
+                    <motion.div
+                        whileHover={{ scale: 1.05 }}
+                        whileTap={{ scale: 0.98 }}
+                        className={`py-5 m-8 cursor-pointer flex bg-white flex-col justify-center  items-center bg-white_primary shadow-std rounded-xl`}>
 
+                        <p className={'text-center px-5 text-h2 font-semibold'}>
+                            Test
+                        </p>
 
-            </div>
+                    </motion.div>
 
+                    <motion.div
+                        whileHover={{ scale: 1.05 }}
+                        whileTap={{ scale: 0.98 }}
+                        className={`py-5 m-8 cursor-pointer flex bg-white flex-col justify-center  items-center bg-white_primary shadow-std rounded-xl`}>
 
+                        <p className={'text-center px-5 text-h2 font-semibold'}>
+                            Test
+                        </p>
 
+                    </motion.div>
+
+                    <motion.div
+                        whileHover={{ scale: 1.05 }}
+                        whileTap={{ scale: 0.98 }}
+                        className={`py-5 m-8 cursor-pointer flex bg-white flex-col justify-center  items-center bg-white_primary shadow-std rounded-xl`}>
+
+                        <p className={'text-center px-5 text-h2 font-semibold'}>
+                            Test
+                        </p>
+
+                    </motion.div>
+
+                </div>
+            </div> 
             <Footer />
         </section>
     );
