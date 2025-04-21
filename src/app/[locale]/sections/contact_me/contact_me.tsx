c619fea134b7d3d844aa380101cc0dc6825bcee6
Michael Guzmán <116075308+AntonioNoguera@users.noreply.github.com>
2025-04-20 22:48:18 -0600

    WIP[Contact Sections] -> Still joggling with layout options

--- src/app/[locale]/sections/contact_me/contact_me.tsx
+++ src/app/[locale]/sections/contact_me/contact_me.tsx
@@ -3,24 +3,30 @@
 import { useTranslations } from "next-intl";
 import React from "react";
 
-import Footer from "@/components/Footer"; 
+import Footer from "@/components/Footer";
 import { motion } from "framer-motion";
+import Image from 'next/image';
+
+import cv from '@png_assets/CV_ICON.png';
+import mail from '@png_assets/MAIL_ICON.png';
 
 export default function ContactSection() {
 
     const t = useTranslations('Contact_Section');
 
     return (
-        <section id="contact" className="min-h-screen  md:h-screen snap-start bg-secondary flex flex-col items-center justify-center pt-14 pb-20 md:pb-0">
+        <section id="contact" className="min-h-screen  md:h-screen snap-start bg-secondary flex flex-col items-center justify-center pt-16 ">
+             
+
+            <div className="flex-grow flex flex-col items-center justify-center w-full">
+
             <p className="text-5xl md:text-8xl lg:text-page_title font-bold text-center text-black_primary px-4 md:px-6 transition-all duration-300 mt-4 lg:mt-0">
                 {t('title')}
             </p>
 
             <p className="text-body2 text-center px-28 pt-6 text-black_primary">
-                    {t('subtitle')}
-                </p>
-
-            <div className="flex-grow flex flex-col items-center justify-center w-full">
+                {t('subtitle')}
+            </p>
 
                 <div className="flex flex-wrap w-full justify-center items- items-stretch">
                     <motion.div
@@ -30,6 +36,13 @@ export default function ContactSection() {
 
                         <p className={'text-center px-5 text-h2 font-semibold'}>
                             Curriculum Vitae
+                            <Image
+                                src={cv}
+                                alt="Una descripción de la imagen"
+                                className=" mx-auto" 
+                                height={100} 
+                                priority
+                            />
                         </p>
 
                     </motion.div>
@@ -41,22 +54,23 @@ export default function ContactSection() {
 
                         <p className={'text-center px-5 text-h2 font-semibold'}>
                             Correo Electrónico
+                            <Image
+                                src={mail}
+                                alt="Una descripción de la imagen"
+                                className=" mx-auto" 
+                                height={100} 
+                                priority
+                            />
                         </p>
                     </motion.div>
+ 
 
-                    <motion.div
-                        whileHover={{ scale: 1.05 }}
-                        whileTap={{ scale: 0.98 }}
-                        className={`py-5 m-8 cursor-pointer flex flex-col justify-center items-center bg-white_primary shadow-std rounded-xl`}>
-
-                        <p className={'text-center px-5 text-h2 font-semibold'}>
-                            Test
-                        </p>
-                    </motion.div>
-
-                </div>
+                </div> 
             </div>
+
+            
             <Footer />
+            
         </section>
     );
 }
