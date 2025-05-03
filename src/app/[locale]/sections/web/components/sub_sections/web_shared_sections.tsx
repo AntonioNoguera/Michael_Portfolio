ae4460a165640a69a7fa211bd0b50a3c50f7dba0
Michael Guzmán <116075308+AntonioNoguera@users.noreply.github.com>
2025-05-02 21:31:59 -0600

    WIP [Linter & shared section]

--- src/app/[locale]/sections/web/components/sub_sections/web_shared_sections.tsx
+++ src/app/[locale]/sections/web/components/sub_sections/web_shared_sections.tsx
@@ -3,32 +3,33 @@
 import { AnimatePresence } from "motion/react"
 import * as motion from "motion/react-client"
 import { useState } from "react"
+import AboutMe from "../../../about_me/about_me"
 
 export default function SharedLayoutAnimation() {
     const [selectedTab, setSelectedTab] = useState(tabs[0])
 
-    let tabMenuClass: string = "flex bg-primary85 mx-20 rounded-2xl ";
-    let tabClass: string = "w-full m-3  flex text-white px-4 py-4  rounded-xl cursor-pointer relative justify-items-center justify-center";
-    let underlineClass: string = "absolute bottom-0 left-0 right-0 h-[2px] bg-[#0f1115] ";
- 
+    let tabMenuClass: string = "flex bg-primary85 mx-20 rounded-2xl px-1";
+    let tabClass: string = "w-full py-4 my-1 flex text-white rounded-xl cursor-pointer relative justify-items-center justify-center";
+    let underlineClass: string = "absolute bottom-0 left-0 right-0 h-[2px] bg-[#00FF0000] ";
+
     return (
-        <div className="flex-grow w-full pt-6">
+        <div className=" w-full h-full flex flex-col pt-6">
             <nav>
                 <ul className={tabMenuClass} >
                     {tabs.map((item) => (
                         <motion.li
-                            key= {item.label}
-                            initial= {false}
-                            animate= {
+                            key={item.title}
+                            initial={false}
+                            animate={
                                 {
                                     backgroundColor: item === selectedTab ? "#eee" : "#eee0",
                                     color: item === selectedTab ? "#000000" : "",
                                 }
                             }
                             className= {tabClass}
-                            onClick={() => setSelectedTab(item)}
-                        >
-                            {`${item.icon} ${item.label}`}
+                            onClick= {() => setSelectedTab(item)}>
+
+                            ${item.title}
 
                             {
                                 item === selectedTab ? (
@@ -39,31 +40,38 @@ export default function SharedLayoutAnimation() {
                     ))}
                 </ul>
             </nav>
-            <main className="flex justify-center">
-                <AnimatePresence mode="wait">
-                    <motion.div
-                        key={selectedTab ? selectedTab.label : "empty"}
-                        initial={{ y: 10, opacity: 0 }}
-                        animate={{ y: 0, opacity: 1 }}
-                        exit={{ y: -10, opacity: 0 }}
-                        transition={{ duration: 0.2 }}
-                        style={icon}
-                    >
-                        {selectedTab ? selectedTab.icon : "😋"}
-                    </motion.div>
-                </AnimatePresence>
-            </main>
+
+            <AnimatePresence mode="wait">
+                <motion.div
+                    className="flex-grow flex justify-center items-center"
+                    key={selectedTab ? selectedTab.title : "empty"}
+                    initial={{ y: 10, opacity: 0 }}
+                    animate={{ y: 0, opacity: 1 }}
+                    exit={{ y: -10, opacity: 0 }}
+                    transition={{ duration: 0.2 }}
+                    style={icon}
+                >
+                    {selectedTab ? selectedTab.body : "😋"}
+                </motion.div>
+            </AnimatePresence>
+
         </div>
     )
-}
+} 
 
 const icon: React.CSSProperties = {
     fontSize: 128,
 }
 
-const allIngredients = [
-    { icon: "🍅", label: "Proyectos" },
-    { icon: "🥬", label: "Habillidades Técnicas" },
+const shared_section = [
+    { 
+        title: "Proyectos", 
+        body: "Proyectos" 
+    },
+    { 
+        title: "Habillidades",
+        body: <AboutMe/>
+    },
 ]
 
-const tabs = allIngredients
+const tabs = shared_section
