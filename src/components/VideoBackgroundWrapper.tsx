a31e618899d66d0e8b42310a85ed196f68bb2327
Michael Guzmán <116075308+AntonioNoguera@users.noreply.github.com>
2025-05-14 01:01:30 -0600

    Feat [Loading Ovelay Animation]

--- src/components/VideoBackgroundWrapper.tsx
+++ src/components/VideoBackgroundWrapper.tsx
@@ -1,7 +1,8 @@
 'use client';
 
-import { useState, useEffect, useRef } from 'react';
+import { useState, useEffect, useRef } from 'react'; 
 import LoadingOverlay from './LoadingOverlay';
+ 
 
 type Props = {
     children: React.ReactNode;
@@ -11,8 +12,10 @@ export default function VideoBackgroundWrapper({ children }: Props) {
     const [loading, setLoading] = useState(true);
     const videoRef = useRef<HTMLVideoElement | null>(null);
 
-    const handleVideoLoaded = () => { 
-        setLoading(false);
+    const handleVideoLoaded = () => {
+        setTimeout(() => {
+            setLoading(false);
+        }, 1000);
     };
 
     useEffect(() => {
@@ -22,17 +25,17 @@ export default function VideoBackgroundWrapper({ children }: Props) {
             if (video.readyState >= 3) {
                 //Video on Cache
                 handleVideoLoaded();
-            } 
+            }
         }
     }, []);
 
     return (
         <>
-            {loading && <LoadingOverlay />}
+            <LoadingOverlay loading={loading} />
             <div className="absolute top-0 left-0 w-full h-screen overflow-hidden z-[-10]">
                 <video
                     ref={videoRef}
-                    className="w-full h-full object-cover"
+                    className="w-full h-full bg-primary object-cover"
                     autoPlay
                     loop
                     muted
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,11 +8,14 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: '#33595E',  
+        primary: '#33595E',
         primary85: '#73979c',
         secondary: '#E9E8FF',
         white_primary: '#F7FDFE',
         black_primary: '#353535',
+      },
+      animation: {
+        'spin-slow': 'spin 1.3s linear infinite',
       },
       fontFamily: {
         sans: ['var(--font-raleway)', 'sans-serif'],
@@ -20,17 +23,17 @@ module.exports = {
       boxShadow: {
         'std': '0 4px 30px 5px rgba(0, 0, 0, 0.25)',
       },
-      screens: { 
+      screens: {
         'lg': '1300px',
       },
-      fontSize: { 
+      fontSize: {
         'body': ['1.20rem'],
         'body2': ['1.340rem'],
-        'body1':  ['1.70rem'],
+        'body1': ['1.70rem'],
         'h3': ['1.80rem'],
         'h2': ['1.95rem'],
         'huge': ['10rem', { lineHeight: '9rem' }],
-        'page_title' : ['20vh', { lineHeight: '18vh'}]
+        'page_title': ['20vh', { lineHeight: '18vh' }]
       },
     },
   },
