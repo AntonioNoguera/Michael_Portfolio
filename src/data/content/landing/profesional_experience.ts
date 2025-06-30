b0c6ce8525536664b611645ee6e13dbd27019965
Michael Guzmán <116075308+AntonioNoguera@users.noreply.github.com>
2025-06-29 23:32:33 -0600

    Feat[New Work Area]

--- src/data/content/landing/profesional_experience.ts
+++ src/data/content/landing/profesional_experience.ts
@@ -4,8 +4,11 @@ import ef from '@svg_assets/svg_enviaflores.svg';
 import pacific from '@png_assets/LOGO_PACIFIC.png';
 import logistic from '@png_assets/LOGISTIC_ICON.png';
 
+import viva from '@svg_assets/viva_logo.png';
+
 import android from '@svg_assets/svg_android.svg';
 import ios from '@svg_assets/svg_apple.svg';
+import mobile from '@png_assets/icon-ios-android.png'
 
 export const UserProfesionalExperience: ProfesionalExperience[] = [
     {
@@ -28,5 +31,12 @@ export const UserProfesionalExperience: ProfesionalExperience[] = [
         companyPeriod: "fulltime_period",
         position: "ios_developer",
         positionIcon: ios,
+    },
+    {
+        companyIcon: viva,
+        companyName: "viva_aerobus_name",
+        companyPeriod: "fulltime_period",
+        position: "mobile_engineer",
+        positionIcon: mobile,
     },
 ]
