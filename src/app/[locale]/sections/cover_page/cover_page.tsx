46fe0cfe26adca39c575cdf8ee888e6e10c99b0c
Michael Noguera <michael.noguera.dev@gmail.com>
2026-01-08 10:13:14 -0600

    Update cover_page.tsx

--- src/app/[locale]/sections/cover_page/cover_page.tsx
+++ src/app/[locale]/sections/cover_page/cover_page.tsx
@@ -5,7 +5,6 @@ import React from "react";
 
 export default function CoverPage() {
 
-    const t = useTranslations('Contact_Section');
     
     return (
         <section id="cover" className="h-screen bg-cover snap-start flex flex-col items-start text-center">
